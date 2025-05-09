37521cf2ba7e77d0546525c24405f16436f94f13
Dung Ta Van <itprono3@gmail.com>
2025-05-09 23:10:20 +0700

    allow font settings

--- plugins/preset-webpage/src/events/index.js
+++ plugins/preset-webpage/src/events/index.js
@@ -1,6 +1,8 @@
 export default (editor) => {
-  editor.on('load', (editor) => {
+  editor.on('load', () => {
+    addFontOptions(editor);
     addZIndexProperty(editor);
+    addFontFacess(editor);
   });
   editor.on('component:add', (component) => {
     if (component.is('image') && !component.getStyle()['object-fit']) {
@@ -72,6 +74,75 @@ function addFlexAttributes(editor) {
       }
     });
   }
+}
+
+function addFontOptions(editor) {
+  const config = editor.getConfig();
+  const fonts = config.fonts;
+  if (!fonts) {
+    return;
+  }
+  const sm = editor.StyleManager;
+  const typographyProps = sm
+    .getSectors()
+    .find(sector => sector.get('id') === 'typography')
+    .get('properties');
+  const fontProp = typographyProps
+    .find(prop => prop.get('property') === 'font-family');
+  if (fontProp) {
+    const currentList = fontProp.get('options') || [];
+    fonts.forEach((font) => {
+      const id = font.id || font.name;
+      const label = font.label || font.displayName || id;
+
+      const alreadyExists = currentList.some(f => f.label === label);
+      if (!alreadyExists) {
+        currentList.push({ id: id, label: label });
+        fontProp.set('options', currentList);
+      }
+    });
+    if (fontProp.view) {
+      fontProp.view.render();
+    }
+  }
+}
+
+function addFontFacess(editor) {
+  const config = editor.getConfig();
+  const fonts = config.fonts;
+  if (!fonts) {
+    return;
+  }
+  const head = editor.Canvas.getDocument().head;
+  const style = document.createElement('style');
+  var html = '';
+  fonts.forEach((font) => {
+    const id = font.id || font.name;
+    const urls = font.sources || font.urls;
+    urls.forEach((item) => {
+      var url = item.src || item.url;
+      var format = font.format;
+      if (!format && url.endsWith('.woff')) {
+        format = 'woff';
+      }
+      if (!format && url.endsWith('.woff2')) {
+        format = 'woff2';
+      }
+      if (!format) {
+        format = 'truetype';
+      }
+      const settings = item.settings || item.config;
+      html += `
+        @font-face {
+          font-family: '${id}';
+          src: url('${url}') format('${format}');
+          ${settings || ''}
+        }
+      `;
+    });
+  });
+  style.innerHTML = html;
+  head.appendChild(style);
 }
 
 function addZIndexProperty(editor) {
