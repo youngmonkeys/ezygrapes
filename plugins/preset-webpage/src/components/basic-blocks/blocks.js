df151d1062824cfdab0a7c11c873ad9607bccce7
Dung Ta Van <itprono3@gmail.com>
2025-09-09 11:21:01 +0700

    update block

--- plugins/preset-webpage/src/components/basic-blocks/blocks.js
+++ plugins/preset-webpage/src/components/basic-blocks/blocks.js
@@ -215,6 +215,8 @@ export default function(editor, opts = {}) {
         <div class="gjs-block-label">${editor.I18n.t('empty_block')}</div>`,
       category: categoryName,
       content: {
+        type: 'div',
+        name: editor.I18n.t('block'),
         style: { 
           'min-height': '120px',
         },
@@ -232,6 +234,7 @@ export default function(editor, opts = {}) {
       category: categoryName,
       content: {
         type: 'text',
+        name: editor.I18n.t('text'),
         content: 'Insert your text here',
         style: { padding: '10px' },
         activeOnRender: 1
@@ -248,6 +251,7 @@ export default function(editor, opts = {}) {
       category: categoryName,
       content: {
         type: 'link',
+        name: editor.I18n.t('link'),
         content: 'Link',
         style: { color: '#d983a6' }
       }
@@ -262,8 +266,9 @@ export default function(editor, opts = {}) {
         <div class="gjs-block-label">${editor.I18n.t('image')}</div>`,
       category: categoryName,
       content: {
-        style: { color: 'black' },
         type: 'image',
+        name: editor.I18n.t('image'),
+        style: { color: 'black' },
         activeOnRender: 1
       }
     });
@@ -278,6 +283,7 @@ export default function(editor, opts = {}) {
       category: categoryName,
       content: {
         type: 'video',
+        name: editor.I18n.t('video'),
         src: 'img/video2.webm',
         style: {
           height: '350px',
@@ -296,6 +302,7 @@ export default function(editor, opts = {}) {
       category: categoryName,
       content: {
         type: 'iframe',
+        name: editor.I18n.t('embed'),
         src: 'img/video2.webm',
         style: {
           height: '315px',
@@ -314,6 +321,7 @@ export default function(editor, opts = {}) {
       category: categoryName,
       content: {
         type: 'map',
+        name: editor.I18n.t('map'),
         style: { height: '350px' }
       }
     });
@@ -326,6 +334,7 @@ export default function(editor, opts = {}) {
       <div class="gjs-block-label">${editor.I18n.t('link_block')}</div>`,
     content: {
       type:'link',
+      name: editor.I18n.t('link_block'),
       editable: false,
       droppable: true,
       style:{
@@ -344,7 +353,9 @@ export default function(editor, opts = {}) {
       </svg>
       <div class="gjs-block-label">${editor.I18n.t('quote')}</div>`,
     category: categoryName,
-    content: `<blockquote class="quote">
+    content: `
+      <blockquote class="quote"
+        data-gjs-custom-name="${editor.I18n.t('quote')}">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ipsum dolor sit
       </blockquote>`
   });
@@ -357,55 +368,17 @@ export default function(editor, opts = {}) {
       </svg>
       <div class="gjs-block-label">${editor.I18n.t('text_section')}</div>`,
     attributes: { class: 'gjs-fonts gjs-f-h1p' },
-    content: `<section class="bdg-sect">
-      <h1 class="heading">Insert title here</h1>
-      <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-      </section>`
-  });
-
-  toAdd('link-block') && bm.add('link-block', {
-    category: categoryName,
-    label: `
-      <svg class="gjs-block-svg" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M3.9,12C3.9,10.29 5.29,8.9 7,8.9H11V7H7A5,5 0 0,0 2,12A5,5 0 0,0 7,17H11V15.1H7C5.29,15.1 3.9,13.71 3.9,12M8,13H16V11H8V13M17,7H13V8.9H17C18.71,8.9 20.1,10.29 20.1,12C20.1,13.71 18.71,15.1 17,15.1H13V17H17A5,5 0 0,0 22,12A5,5 0 0,0 17,7Z"></path>
-      </svg>
-      <div class="gjs-block-label">${editor.I18n.t('link_block')}</div>`,
-    content: {
-      type:'link',
-      editable: false,
-      droppable: true,
-      style:{
-        display: 'inline-block',
-        padding: '5px',
-        'min-height': '50px',
-        'min-width': '50px'
-      }
-    },
-  });
-
-  toAdd('quote') && bm.add('quote', {
-    label: `
-      <svg class="gjs-block-svg" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M14,17H17L19,13V7H13V13H16M6,17H9L11,13V7H5V13H8L6,17Z"></path>
-      </svg>
-      <div class="gjs-block-label">${editor.I18n.t('quote')}</div>`,
-    category: categoryName,
-    content: `<blockquote class="quote">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ipsum dolor sit
-      </blockquote>`
-  });
-
-  toAdd('text-basic') && bm.add('text-basic', {
-    category: categoryName,
-    label: `
-      <svg class="gjs-block-svg" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M21,6V8H3V6H21M3,18H12V16H3V18M3,13H21V11H3V13Z"></path>
-      </svg>
-      <div class="gjs-block-label">${editor.I18n.t('text_section')}</div>`,
-    attributes: { class: 'gjs-fonts gjs-f-h1p' },
-    content: `<section class="bdg-sect">
-      <h1 class="heading">Insert title here</h1>
-      <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+    content: `
+      <section class="bdg-sect"
+        data-gjs-custom-name="${editor.I18n.t('text_section')}">
+        <h1 class="heading"
+          data-gjs-custom-name="${editor.I18n.t('heading')}">
+            Insert title here
+          </h1>
+        <p class="paragraph"
+          data-gjs-custom-name="${editor.I18n.t('paragraph')}">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </p>
       </section>`
   });
 
@@ -424,10 +397,23 @@ export default function(editor, opts = {}) {
     attributes: { class: 'gjs-fonts gjs-f-list' },
     content: {
       tagName: 'ul',
+      name: editor.I18n.t('unordered_list'),
       components: [
-        { tagName: 'li', content: 'List item 1' },
-        { tagName: 'li', content: 'List item 2' },
-        { tagName: 'li', content: 'List item 3' },
+        {
+          tagName: 'li',
+          name: editor.I18n.t('item'),
+          content: 'List item 1'
+        },
+        {
+          tagName: 'li',
+          name: editor.I18n.t('item'),
+          content: 'List item 2'
+        },
+        {
+          tagName: 'li',
+          name: editor.I18n.t('item'),
+          content: 'List item 3'
+        },
       ]
     }
   });
@@ -447,10 +433,23 @@ export default function(editor, opts = {}) {
     attributes: { class: 'gjs-fonts gjs-f-ol' },
     content: {
       tagName: 'ol',
+      name: editor.I18n.t('ordered_list'),
       components: [
-        { tagName: 'li', content: 'Step 1' },
-        { tagName: 'li', content: 'Step 2' },
-        { tagName: 'li', content: 'Step 3' },
+        {
+          tagName: 'li',
+          name: editor.I18n.t('item'),
+          content: 'Step 1'
+        },
+        {
+          tagName: 'li',
+          name: editor.I18n.t('item'),
+          content: 'Step 2'
+        },
+        {
+          tagName: 'li',
+          name: editor.I18n.t('item'),
+          content: 'Step 3'
+        },
       ]
     }
   });
