6869199e081bfc5b6e8007a3c31fea1f28b8e58a
tvd12 <ta.van.dung@youngmonkeys.org>
2026-07-02 22:32:09 +0700

    update

--- plugins/preset-webpage/webpack.prod.js
+++ plugins/preset-webpage/webpack.prod.js
@@ -3,4 +3,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  // No source maps in the production bundle: the .map file isn't served in
+  // some deployment environments, and a sourceMappingURL comment pointing
+  // at a missing .map causes a 404 there.
+  devtool: false,
 });
